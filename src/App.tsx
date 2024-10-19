918675a0c9a59923e5a9e5720d3f3ff4457c2481
yasfoyt <yasfoyt@gmail.com>
2024-10-19 05:28:13 -0700

    commit 03

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Slider from "./components/Slider";
 import { Teachers } from "./components/BestTeachers";
 import Subjects from "./components/Subjects";
 import Courses from "./components/Courses";
-import Books from "./components/books";
+import Books from "./components/Books";
 import Features from "./components/Features";
 import Questions from "./components/questions";
 import Ready from "./components/Ready";
